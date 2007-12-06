46fe5315f1597c401e4ba0fa36fc64e1cb9d1ac5
J Brandon Keith <jbrkeith@gmail.com>
2007-12-06 21:00:37 +0000

    git-svn-id: svn://danse.us/MolDyn/molDynamics@3 a3a1b2a8-efeb-4e4f-91fb-0cb55f2ba858

--- fft/example.py
+++ fft/example.py
@@ -1,5 +1,13 @@
-#open file
+from scipy import *
+from pylab import *
 
+a = zeros((1000,1000))
+a[:100][:100]=1
+
+
+b = fft(a)
+plot(abs(b))
+show()
 # do spatial fourier transform
 
 # do time fourier transform
