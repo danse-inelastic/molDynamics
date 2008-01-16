fixing the scaling problem of the partition function on the entropy calculator
git-svn-id: svn://danse.us/MolDyn/molDynamics@68 a3a1b2a8-efeb-4e4f-91fb-0cb55f2ba858

experimental/entropyCalculator.py
@@ -3,14 +3,19 @@
 #--- User setup ---------------------------------------------------
 sym  = 'Ni'
 temps=['0345','0691','1036','1382']
-files=[temp + '/' + sym + '.conv.gout' for temp in temps]
+#files=[temp + '/' + sym + '.conv.gout' for temp in temps]
 
 #--- OR: ---
-# files=[ sym + '.' + str(int(temp)) + '.conv.gout' for temp in temps]
+from os import sep
+baseDir='/home/jbk/gulp3.0/z_py/Ni'+sep
+temps=['345','691','1036','1382']
+files=[ baseDir+sym + '.' + str(int(temp)) + '.conv.gout' for temp in temps]
 
 #--- Imports ------------------------------------------------------
 import numpy as np
+import math
 kb= 8.617343E-5 # eV/K
+h = 4.1356673328075734E-15 # eV * s
 
 #--- File parsing -------------------------------------------------
 def getEnergies(fileName):
@@ -34,15 +39,20 @@ def getEnergies(fileName):
 
 F=[]
 U=[]
+eVToMegaeV=1e-6
+N=864
+statMechNormalizer= -3*N*math.log(h) - (1/2.*math.log(2*math.pi*N) + N*math.log(N) - N)
 # loop over different temperatures
 for f,T in zip(files,temps):
     T = float(T)
     #extract energies from gulp outputs
     energies,times=getEnergies(f)
+    normalizedEns=energies/(kb*T)*eVToMegaeV
+    boltzFactor=np.exp(normalizedEns)
     #calculate Q's and F's at each temperature
-    Q =       np.sum( np.exp(energies/(kb*T)) )    #/float( len(energies) )
-    U.append( np.dot( energies ,np.exp(energies/(kb*T)) )/Q) #/float( len(energies) )
-    F.append(kb*T*np.log(Q))
+    Q =       np.sum( boltzFactor )    #/float( len(energies) )
+    U.append( np.dot( energies , boltzFactor )/Q) #/float( len(energies) )
+    F.append(kb*T*(np.log(Q)+statMechNormalizer))
 
 # interpolate to get F -- should equal energy of structure at absolute 0
 #   (do quick minimization to find out)