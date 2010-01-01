f7e45256474a0600c3f4e46049745552b534cf3e
J Brandon Keith <jbrkeith@gmail.com>
2010-01-01 20:25:07 +0000

    have a mostly-functioning dds storage demo for potential container files associated with atomsim settings
    git-svn-id: svn://danse.us/MolDyn/molDynamics@672 a3a1b2a8-efeb-4e4f-91fb-0cb55f2ba858

--- examples/gulp/StorePotentialDevDb.py
+++ examples/gulp/StorePotentialDevDb.py
@@ -15,21 +15,46 @@ from dsaw.model.visitors.OrmManager import OrmManager
 orm = OrmManager(db, guid)
 
 from memd.gulp.GulpPotential import GulpPotential
+from vnfb.components import dds
 #gulpPotential = GulpPotential()
 #gulpPotential.potential_name = 'first'
 #orm.save(gulpPotential)
 import os
 for potentialName in os.listdir('originalPotentials'):
-    f = file(potentialName)
-    f.read()
+    if potentialName[0]=='.': continue
+    f = file(os.path.join('originalPotentials',potentialName))
+    librarycontent = f.read()
+    ddsObj = dds()
+    
+    potential_filename = potentialName
+    try:
+#        gulpPotential = director.clerk.getRecordByID(GulpPotential, self.inventory.potential_name, 
+#            associatedDataFileToVerify = self.inventory.potential_filename, director=director)
+        1/0
+        gulpPotential = orm.load(GulpPotential, id = '1')
+    except:
+        # if it doesn't exist, create a new one from this entry
+        #gulpPotential = director.clerk.newDbObject(GulpPotential, id = self.inventory.potential_name)
+        gulpPotential = GulpPotential()
+        gulpPotential.filename = potential_filename
+        orm.save(gulpPotential)
+        
+        #put the potential in the potentials subdirectory
+        libfile = ddsObj.abspath(gulpPotential, filename = potential_filename)
+        libDirectory,file = os.path.split(libfile)
+        if not os.path.exists(libDirectory):
+            try:
+                os.makedirs(libDirectory)
+            except Exception, err:
+                raise RuntimeError, "unable to create directory %r. %s: %s" % (
+                    self.path, err.__class__.__name__, err)
+        open(libfile, 'w').write(librarycontent)
+        #server = director.clerk.dereference(job.server)
+        ddsObj.remember(gulpPotential, files=[potential_filename])
+        break
     
     
-    gulpPotential = GulpPotential()
-    baseName,ext = os.path.splitext(potentialName)
-    gulpPotential.potential_name = baseName
-    gulpPotential.filename = potentialName
-    g
-    orm.save(gulpPotential)
+
 
 gulpPotential2 = orm.load(GulpPotential, id = orm(gulpPotential).id)
 print gulpPotential2.potential_name
