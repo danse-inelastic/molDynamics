e28b5911fc69d4feacf4b89cde23e1efd0d44714
J Brandon Keith <jbrkeith@gmail.com>
2010-01-12 01:27:08 +0000

    adding creator to DO since it overrides interface creator regardless and i need to set creator in my routines
    git-svn-id: svn://danse.us/MolDyn/molDynamics@695 a3a1b2a8-efeb-4e4f-91fb-0cb55f2ba858

--- src/memd/gulp/GulpSettings.py
+++ src/memd/gulp/GulpSettings.py
@@ -17,14 +17,14 @@ from dsaw.model.Inventory import Inventory as InvBase
 
 class GulpSettings:
     #input
-    #matter = Structure() HACK--temporarily remove
-    runtype = 'md'
+    matter = Structure() 
+    runtype = ''
     dos_projection = [0.0]
     potential = GulpPotential()
     inputFile = 'gulp.gin'
     
     short_description = ''
-#    creator = ''
+    creator = ''
 #    import time
 #    date = time.ctime()
 #    results_state = ''
@@ -40,14 +40,14 @@ class GulpSettings:
             setattr(self, k, v)
            
     class Inventory(InvBase):
-        #structure = InvBase.d.reference(name='structure', targettype=Structure, owned=False)
-        runtype = InvBase.d.str(name = 'runtype', max_length = 80, default ="md")
+        matter = InvBase.d.reference(name='matter', targettype=Structure, owned=False)
+        runtype = InvBase.d.str(name = 'runtype', max_length = 80)
         dos_projection = InvBase.d.array(name='dos_projection', elementtype='float', shape=1)
         potential = InvBase.d.reference(name='potential', targettype=GulpPotential, owned=False)
-        short_description = InvBase.d.str(name = 'short_description', max_length = 80, default ="")
+        short_description = InvBase.d.str(name = 'short_description', max_length = 80)
         inputFile = InvBase.d.str(name = 'inputFile', max_length = 80, default ="gulp.gin")
         
-#        creator = InvBase.d.str(name = 'creator', max_length = 80, default ="")
+        creator = InvBase.d.str(name = 'creator', max_length = 80)
 #        date = InvBase.d.date(name = 'date')
 #        results_state = InvBase.d.str(name='results_state', length=16, default='')
         compressed_xyzTrajectory_filename = InvBase.d.str(name = 'compressed_xyzTrajectory_filename', 
