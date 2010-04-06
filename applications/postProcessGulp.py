99bdda4f73beeadcd33809f6815bf4c7037210ce
J Brandon Keith <jbrkeith@gmail.com>
2010-04-06 23:57:22 +0000

    in the middle of writing trajectory processor
    git-svn-id: svn://danse.us/MolDyn/molDynamics@768 a3a1b2a8-efeb-4e4f-91fb-0cb55f2ba858

--- applications/postProcessGulp.py
+++ applications/postProcessGulp.py
@@ -33,6 +33,11 @@ class PostProcessGulp(Script):
         #params
         gulpOutputFile = inv.str('gulpOutputFile', default='gulp.gout')
         vibrationsFile = inv.str('vibrationsFile', default='vibrations.xyz')
+        #task
+        thinTrajectory = inv.bool('thinTrajectory', default=False)
+        #
+        oldTrajectory = inv.str('oldTrajectory', default='gulp.nc')
+        newTrajectory = inv.str('newTrajectory', default='gulp2.nc')
 
     def __init__(self):
         Script.__init__(self, 'PostProcessGulp')
@@ -50,6 +55,21 @@ class PostProcessGulp(Script):
             c.setNetcdfFile(self.ncFile)
             c.convert()
             os.remove(self.historyFile)
+        if self.inventory.thinTrajectory:
+            #actually, all i need to do here is just create new time dimension and then
+            #create new variables for kinetic energy, etc....but first be sure to just run normal
+            #gulp at 60,80K to look for signs of vacacy diffusion (where quantum correction doesn't matter so much)
+#            from Scientific.IO.NetCDF import NetCDFFile
+#            self.file = NetCDFFile(self.inventory.oldTrajectory, 'r')
+#            self.file2 = NetCDFFile(self.inventory.newTrajectory, 'w')
+#            for key,val in self.file.dimensions.iteritems():
+#                self.file2.createDimension(key, val)
+#                
+#            for key,val in self.file.variables.iteritems():
+#                self.file2.createVariable(key, val, )
+#            from vsat.Trajectory import Trajectory
+#            trajOriginal=Trajectory()
+            
         if self.inventory.serializePhononArrays:
             from memd.gulp.output.OutputParser import OutputParser
             o = OutputParser(self.gulpOutputFile, runtype = 'phonons')
