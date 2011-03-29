cc1805c2786bc9b40a3cfbaee4a7e5cfbb008c18
J Brandon Keith <jbrkeith@gmail.com>
2011-03-29 22:14:50 +0000

    * tests on all the default api of gulp bindings
    git-svn-id: svn://danse.us/MolDyn/molDynamics@907 a3a1b2a8-efeb-4e4f-91fb-0cb55f2ba858

--- buildbot/TestMemd.py
+++ buildbot/TestMemd.py
@@ -6,7 +6,7 @@ import unittest
 import numpy
 
 # useful variables
-thisfile = locals().get('__file__', 'TestAtomSimulation.py')
+thisfile = locals().get('__file__', 'TestMemd.py')
 tests_dir = os.path.dirname(os.path.abspath(thisfile))
 #testdata_dir = os.path.join(tests_dir, 'testdata')
 
@@ -15,7 +15,7 @@ sys.path.insert(0,os.path.abspath('..'))# this should put the source code first 
 engineTest=False
 lim=100
 
-class TestAtomSimulation(unittest.TestCase):
+class TestMemd(unittest.TestCase):
     """test gulp and mmtk bindings...note we remove first 100 characters to get rid of date info
     ...also note we have a switch to also test parnasis engine above
     """
@@ -30,30 +30,31 @@ class TestAtomSimulation(unittest.TestCase):
         m.forcefield='axiallySymmetricNWS.lib'
         m.inputDeckName = 'mOpt.gin'
         m.writeInputfile(tests_dir)
-        correctInput="""#
-# parnasis data
-# Created by: jbk
-# Date of creation: Mon Mar 21 11:18:33 2011
-#
-q_algorithm = 'random_gen' 
-q_type = 'powder average' 
-q_resolution = 0.1 
-trajectory = ['5MF6SB7Q/gulp.nc'] 
-time_steps_sampled = (0, 165, 1) 
-selected_atoms = {'Au': ['*']} 
-q_range = [0, 2] 
-short_description = 'EISF' 
-weights = 'incoherent' 
-q_directions = [True, True, True] 
-type = 'eisf' 
-output_files = {'eisf': 'EISF_gulp.plot'}"""
+        correctInput="""optimise 
+vectors
+2.456 0.0 0.0
+0.0 2.456 0.0
+0.0 0.0 6.696
+cartesian
+C   0 0 0
+C   0 0 3.348
+C   0.708986 1.228 0
+C   1.41797 0 3.348
+harmonic 
+C C 22.594 0.63952 0.0 0.0 1.8
+C C  8.301 3.42180 0.0 1.8 2.6
+C C -0.231 24.95086 0.0 2.6 3.1
+C C  0.362 2.94862 0.0 3.1 3.5
+                
+output movie xyz molDynamics.xyz
+dump molDynamics.res"""
         # this tests input file
         inputContents=file('mOpt.gin').read().strip()
         #print correctInput[lim:]
         #print inputContents[lim:]
         assert correctInput[lim:]==inputContents[lim:]
         # this tests whether parnasis runs with this input file
-        if engineTestTest:
+        if engineTest:
             os.system('gulp < mdOpt.gin > mdOpt.gout')
 
     def testPhononApi(self):
@@ -63,20 +64,26 @@ output_files = {'eisf': 'EISF_gulp.plot'}"""
         m.forcefield='axiallySymmetricNWS.lib'
         m.inputDeckName = 'mPhon.gin'
         m.writeInputfile(tests_dir)
-        correctInput="""#
-# parnasis data
-# Created by: jbk
-# Date of creation: Mon Mar 21 11:59:33 2011
-#
-trajectory = ['5MF6SB7Q/gulp.nc'] 
-smoothing_window = 1 
-time_steps_sampled = (0, 165, 1) 
-selected_atoms = {'Au': ['*']} 
-weights = 'mass' 
-short_description = 'DOS calculation' 
-smoothing_type = 'hanning' 
-type = 'mddos' 
-output_files = {'dos': 'DOS_gulp.plot'}"""
+        correctInput="""phonon 
+vectors
+2.456 0.0 0.0
+0.0 2.456 0.0
+0.0 0.0 6.696
+cartesian
+C   0 0 0
+C   0 0 3.348
+C   0.708986 1.228 0
+C   1.41797 0 3.348
+harmonic 
+C C 22.594 0.63952 0.0 0.0 1.8
+C C  8.301 3.42180 0.0 1.8 2.6
+C C -0.231 24.95086 0.0 2.6 3.1
+C C  0.362 2.94862 0.0 3.1 3.5
+                
+shrink 
+project 0
+output phonon 
+output frequency"""
         # this tests input file
         inputContents=file('mPhon.gin').read().strip()
         assert correctInput[lim:]==inputContents[lim:] 
@@ -91,23 +98,33 @@ output_files = {'dos': 'DOS_gulp.plot'}"""
         m.forcefield='axiallySymmetricNWS.lib'
         m.inputDeckName = 'mMd.gin'
         m.writeInputfile(tests_dir)
-        correctInput="""#
-# parnasis data
-# Created by: jbk
-# Date of creation: Mon Mar 21 19:19:38 2011
-#
-diffusion_time_steps = 'All' 
-dimensions = 3 
-trajectory = ['5MF6SB7Q/gulp.nc'] 
-time_steps_sampled = (0, 165, 1) 
-selected_atoms = {'Au': ['*']} 
-weights = 'mass' 
-short_description = 'mean squared displacement' 
-type = 'msddiffusion' 
-projection_vector = 'None' 
-output_files = {'diffusion': 'DIFFUSION_gulp.plot', 'msd': 'MSD_gulp.plot'}"""
+        correctInput="""md conv 
+vectors
+2.456 0.0 0.0
+0.0 2.456 0.0
+0.0 0.0 6.696
+cartesian
+C   0 0 0
+C   0 0 3.348
+C   0.708986 1.228 0
+C   1.41797 0 3.348
+harmonic 
+C C 22.594 0.63952 0.0 0.0 1.8
+C C  8.301 3.42180 0.0 1.8 2.6
+C C -0.231 24.95086 0.0 2.6 3.1
+C C  0.362 2.94862 0.0 3.1 3.5
+                
+ensemble nvt 0.05
+temperature 0.0
+equilibration 0.0 ps
+production 0.0 ps
+timestep 0.002 ps
+sample 5.0 ps
+output history molDynamics.his.his
+write 0.0 ps
+dump molDynamics.res"""
         # this tests input file
-        inputContents=file('msddiffusion.inp').read().strip()
+        inputContents=file('mMd.gin').read().strip()
         assert correctInput[lim:]==inputContents[lim:] 
         # this tests whether parnasis runs with this input file
         if engineTest:
