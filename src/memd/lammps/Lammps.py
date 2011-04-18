af02ebaa47fe46a42d49ccdaa2f4e29ca794dc5c
J Brandon Keith <jbrkeith@gmail.com>
2011-04-18 17:55:11 +0000

    * more fixes on the lammps bindings
    git-svn-id: svn://danse.us/MolDyn/molDynamics@923 a3a1b2a8-efeb-4e4f-91fb-0cb55f2ba858

--- src/memd/lammps/Lammps.py
+++ src/memd/lammps/Lammps.py
@@ -1,5 +1,18 @@
 from memd.MolDynamics import MolDynamics
 from os import linesep
+import os
+
+def nNone(opt):
+    if opt:
+        return ' '+opt
+    else:
+        return ''
+    
+def nDeflt(opt, deflt):
+    if opt!=deflt:
+        return opt+linesep
+    else:
+        return ''
 
 class Lammps(MolDynamics):
     """Lammps engine for MolDynamics interface.  
@@ -12,12 +25,15 @@ class Lammps(MolDynamics):
         
     def getInputfile(self,o):
         lines=''
-        lines+=o.units+linesep
+        lines+='units '+o.units+linesep
+        lines+='atom_style '+o.atom_style+nNone(o.atom_style_args)
+        lines+=nDeflt(o.dimensions)
+        lines+='lattice '+o.lattice_style+nNone(o.scale_factor_between_lattice_and_simulation_box)+nNone(o.lattice_args)+linesep
         if True:#o.how_to_input_atoms=='specify atom creation parameters':
             #print dir(o)
             for region in o.lammpsregion_set.all():
                 lines+='region '+region.region_id+' '+region.style+' '+region.args+linesep
-                lines+='create_box '+str(len(o.structure.getMatterObject().getSpecies()))+\
+                lines+='create_box '+region.number_of_atom_species_in_region+\
                     ' '+region.region_id+linesep
     #        lattice = o.structure.lattice
     #        al,be,ga=lattice.abcABG[3:]
@@ -26,14 +42,16 @@ class Lammps(MolDynamics):
     #            lines+='create_box '+str(len(o.structure.getSpecies()))+' '+o.region_name+linesep
     #        else:
     #            lines+=''
-            lines+='create_atoms '+o.number_of_atom_species_to_create+' '+o.style_of_atom_creation+\
-                ' '+o.style_of_atom_creation_args
+            lines+='create_atoms '+str(o.number_of_atom_species_to_create)+' '+o.style_of_atom_creation+\
+                ' '+o.style_of_atom_creation_args+linesep
             lines+='mass '+o.atom_masses+linesep
-            lines+='velocity '+o.style_of_atom_velocity+' '+o.style_of_atom_velocity_options+linesep
+            lines+='velocity '+o.style_of_atom_velocity+' '+o.style_of_atom_velocity_args+linesep
         else:
             pass
+        for prop in o.lammpsatomproperties_set.all():
+            lines+='set '+prop.type_of_atom_group+' '+prop.group_id+' '+p
         lines+='pair_style '+o.pair_interaction_potential+' '+o.pair_interaction_options+linesep
-        lines+='pair_coeff '+o.pair_interaction_coefficients
+        lines+='pair_coeff '+o.pair_interaction_coefficients+linesep
         if o.skin_distance_for_pairwise_neighbor_lists!='default':
             lines+='neighbor '+o.skin_distance_for_pairwise_neighbor_lists+o.neighbor_list_algorithm+linesep
         lines+='neigh_modify '+o.neighbor_list_options+' '+o.neighbor_list_option_args+' '
@@ -44,20 +62,22 @@ class Lammps(MolDynamics):
         lines+=linesep
         # fix commands
         for fix in o.lammpsfix_set.all():
-            lines+=fix.fix_id+' '+fix.group_id+' '+fix.style+' '+fix.options+linesep
+            lines+='fix '+fix.fix_id+' '+fix.group_id+' '+fix.style+' '+fix.args+linesep
         for dump in o.lammpsdump_set.all():
-            lines+='dump '+dump.dump_id+' '+dump.style+' '+dump.style_args+' '+\
-                dump.dump_every_n_timesteps+' '+dump.dump_filename+linesep
-        lines+='thermo '+o.output_thermodynamics_every_n_timesteps+linesep
+            lines+='dump '+dump.dump_id+' '+dump.group_id+' '+dump.style+' '+dump.style_args+' '+\
+                str(dump.dump_every_n_timesteps)+' '+dump.dump_filename+linesep
+        lines+='thermo '+str(o.output_thermodynamics_every_n_timesteps)+linesep
         # execution
-        lines+='run '+o.number_of_timesteps+' '+o.run_options+' '+o.run_option_args+linesep
-        
+        if o.run_options:
+            lines+='run '+str(o.number_of_timesteps)+' '+o.run_options+' '+o.run_option_args+linesep
+        else:
+            lines+='run '+str(o.number_of_timesteps)+linesep
         return lines
     
-    def writeInputfile(self,o):
+    def writeInputfile(self,o,workingDir):
         contents = self.getInputfile(o)
         #now write them
-        file(o.input_files.split()[0],'w').write(contents)
+        file(os.path.join(workingDir,o.input_files.split()[0]),'w').write(contents)
         
     def _ind(self,list):
         """gives the indices of the list to iterate over"""
